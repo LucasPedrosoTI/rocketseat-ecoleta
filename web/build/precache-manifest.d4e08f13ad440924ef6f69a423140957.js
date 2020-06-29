e763484d1d8ab9a75782e3553a86a6d5ba9dc569
lucaspedrosoti <lps1010@gmail.com>
2020-06-29 15:26:06 -0300

    web build

---
+++ web/build/precache-manifest.d4e08f13ad440924ef6f69a423140957.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34067cd0b1b0790f43b9a1958a63ccc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bef3cc3bacf782d6a57",
+    "url": "/static/css/main.d444742a.chunk.css"
+  },
+  {
+    "revision": "c10719053388db7915dd",
+    "url": "/static/js/2.e9dca4e3.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.e9dca4e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bef3cc3bacf782d6a57",
+    "url": "/static/js/main.349665d5.chunk.js"
+  },
+  {
+    "revision": "1e184bed41a810564677",
+    "url": "/static/js/runtime-main.1ca79223.js"
+  },
+  {
+    "revision": "fe2283e1f17af7f6d2889b5692937968",
+    "url": "/static/media/home-background.fe2283e1.svg"
+  },
+  {
+    "revision": "47c4581a1dc536c1ba57fddafd6671b9",
+    "url": "/static/media/logo.47c4581a.svg"
+  }
+]);
